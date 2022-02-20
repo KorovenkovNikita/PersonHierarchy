4b2355d2b3112d1593414f3b1191aa4f51a95f79
KorovenkovNikita <korovenkov.nik@mail.ru>
2022-02-20 17:14:06 +0300

    Adding a constant EMPTY

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
+    private static final String EMPTY = "-";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,14 +12,14 @@ public class Main {
         Person[] persons = new Person[N];
         for (int i = 0; i < N; i++) {
             String[] line = sc.nextLine().split(";");
-            if (Objects.equals(line[4], "-") && Objects.equals(line[5], "-")) {
+            if (Objects.equals(line[4], EMPTY) && Objects.equals(line[5], EMPTY)) {
                 persons[i] = new Person(line);
                 continue;
             }
-            if (Objects.equals(line[4], "-")) {
+            if (Objects.equals(line[4], EMPTY)) {
                 persons[i] = new Employee(line);
             }
-            if (Objects.equals(line[5], "-")) {
+            if (Objects.equals(line[5], EMPTY)) {
                 persons[i] = new Student(line);
             }
         }
